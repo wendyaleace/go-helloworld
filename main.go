93d9709a8b2c707ed604ba0be01fc186e980bd61
Wendy Warner <wendy@Nancys-MacBook-Pro.local>
2024-01-12 20:11:32 -0800

    update numbas

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{8, 10, 209}
+	nums := []int{9, 19, 209, 569}
 	for _, num := range nums {
 		fmt.Println(num, "plus 10 = ", addTen(num))
 	}
