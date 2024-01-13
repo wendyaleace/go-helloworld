3b4f05f33d8f7a94edc772a8f841d663da3d4838
Dana Woodman <dana@danawoodman.com>
2024-01-12 20:19:48 -0800

    change numbers

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{9, 19, 209, 569}
+	nums := []int{39, 4323, 2, 569}
 	for _, num := range nums {
 		fmt.Println(num, "plus 10 = ", addTen(num))
 	}
